ceff2bfb2f69ffff9f27921e8ea04cb2ccbfc004
gs495a <gs495a@att.com>
2018-09-06 10:44:49 -0500

    Dataset Management
    Added CMLP dataset CRUD operation without implemenation
    Added documentation
    Added sub module for dataset-service
    Added sub module for datasource-service
    
    Issue-ID: ACUMOS-1182
    
    Change-Id: I81414d7b83fa8ac3ea9a141cd926504e39eba59e
    Signed-off-by: gs495a <gs495a@att.com>

---
+++ dataset-service/src/main/java/org/acumos/service/common/HelperTool.java
@@ -0,0 +1,159 @@
+/*-
+ * ===============LICENSE_START=======================================================
+ * Acumos
+ * ===================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ===================================================================================
+ * This Acumos software file is distributed by AT&T
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============LICENSE_END=========================================================
+ */
+
+package org.acumos.service.common;
+
+/**
+ * <p>
+ * Helper class to maintain application level resources
+ * </p>
+ * 
+ * @author am375y
+ */
+public class HelperTool {
+
+	/*
+	private static Logger log = LoggerFactory.getLogger(HelperTool.class);
+
+	private static String apiVersion;
+	private static String resourceURL;
+	private static final String MESSAGE = "getComponentPropertyValue(), trying to find value for property ";
+	private static final String REMOTEMESSAGE = "getRemoteUser(), trying to find remote user for request ";
+	
+	private HelperTool() {
+		super();
+	}
+
+	
+	public static String getComponentPropertyValue(String key) throws IOException {
+		log.info(MESSAGE + key);
+		Properties prop = new Properties();
+		InputStream input;
+		if (new File(System.getProperty("user.dir") + System.getProperty("file.separator") + "config.properties")
+				.exists()) {
+			log.info(MESSAGE + key
+					+ " from custom config file.");
+			input = new FileInputStream(
+					System.getProperty("user.dir") + System.getProperty("file.separator") + "config.properties");
+		} else {
+			log.info(MESSAGE + key
+					+ " from provided config file.");
+			input = HelperTool.class.getResourceAsStream("/config.properties");
+		}
+		prop.load(input);
+		return prop.getProperty(key);
+	}
+
+	
+	public static String getRemoteUser(HttpServletRequest request) throws CmlpDataSrcException {
+		log.info(REMOTEMESSAGE + request.getRequestedSessionId());
+		if (request.getRemoteUser() != null) {
+			log.info(REMOTEMESSAGE + request.getRequestedSessionId() + " as "
+					+ request.getRemoteUser());
+			return request.getRemoteUser();
+		}
+
+		if (request.getUserPrincipal() != null) {
+			log.info(REMOTEMESSAGE + request.getRequestedSessionId() + " as "
+					+ request.getRemoteUser() + " using principal " + request.getUserPrincipal());
+			return request.getUserPrincipal().getName();
+		}
+		String authorization = request.getHeader("Authorization");
+
+		log.info(REMOTEMESSAGE + request.getRequestedSessionId()
+				+ "after fetching failed using principal.");
+		if (authorization != null && authorization.startsWith("Basic")) {
+			String base64Credentials = authorization.substring("Basic".length()).trim();
+			String credentials = new String(Base64.getDecoder().decode(base64Credentials), Charset.forName("UTF-8"));
+			final String[] values = credentials.split(":", 2);
+			log.info(REMOTEMESSAGE + request.getRequestedSessionId() + " as "
+					+ values[0] + "after decoding.");
+			return values[0];
+		}else {
+			String[] variables = { "Authorization" };
+
+			CmlpRestError err = CmlpErrorList.buildError(ErrorListEnum.E_1001, variables, null,
+					CmlpApplicationEnum.DATASOURCE);
+
+			throw new CmlpDataSrcException(
+					"Unauthorized-Missing authorization parameter. Please send all the required information.",
+					Status.UNAUTHORIZED.getStatusCode(), err);
+		}
+	}
+
+	
+	public static String getEnv(String envKey, String defaultValue) {
+		String value = System.getenv(envKey);
+		log.info("getEnv(), environment variable value for envkey " + envKey + "has been searched.");
+		if (value == null) {
+			log.info("getEnv(), setting value to environment variable value for envkey: " + envKey);
+			value = System.getProperty(envKey);
+		}
+		if (value == null) {
+			log.info("getEnv(), setting value to the default value passed for envkey: " + envKey);
+			value = defaultValue;
+		}
+		return value;
+	}
+
+	public static String getAPIVersion(HttpServletRequest request) {
+		if (apiVersion == null) {
+			String requestURI = request.getRequestURI();
+			String cntxtPath = request.getContextPath();
+			String resourceURI = requestURI.substring(cntxtPath.length() + 1);
+			apiVersion = resourceURI.substring(0, resourceURI.indexOf('/'));
+		}
+		return apiVersion;
+	}
+
+	public static String getAPIVersion() {
+		return apiVersion;
+	}
+
+	public static String getResourceURL(HttpServletRequest request) {
+		try {
+			if (resourceURL == null) {
+				String requestURL = request.getRequestURL().toString();
+				String requestURI = request.getRequestURI().toString();
+
+				String baseURL = requestURL.substring(0, requestURL.indexOf(requestURI));
+				String ingressPath = HelperTool.getEnv("ingress_service_path",
+						HelperTool.getComponentPropertyValue("ingress_service_path"));
+
+				StringBuilder sb = new StringBuilder();
+				sb.append(baseURL).append("/").append(ingressPath).append(requestURI);
+
+				resourceURL = sb.toString();
+
+				log.info("requestURL: " + requestURL);
+				log.info("requestURI: " + requestURI);
+				log.info("baseURL: " + baseURL);
+				log.info("ingress_path: " + ingressPath);
+				log.info("ResourceURL: " + resourceURL);
+			}
+		} catch (Exception e) {
+			log.error("Error: ", e);
+			return request.getRequestURL().toString();
+		}
+
+		return resourceURL;
+	}
+	*/
+}
---
+++ dataset-service/src/main/java/org/acumos/service/transport/AuthorTransport.java
@@ -0,0 +1,83 @@
+/*-
+ * ===============LICENSE_START=======================================================
+ * Acumos
+ * ===================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+ * ===================================================================================
+ * This Acumos software file is distributed by AT&T and Tech Mahindra
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============LICENSE_END=========================================================
+ */
+
+package org.acumos.service.transport;
+
+import java.util.Objects;
+
+/**
+ * Trivial model to transport a pair of strings.
+ */
+public class AuthorTransport {
+	private String name;
+	private String contact;
+
+	/**
+	 * Builds an empty object.
+	 */
+	public AuthorTransport() {
+		// no-arg constructor
+	}
+
+	/**
+	 * Builds an object with the specified values.
+	 * 
+	 * @param name
+	 *            name to transport.
+	 * @param contact
+	 *            contact to transport.
+	 */
+	public AuthorTransport(String name, String contact) {
+		this.name = name;
+		this.contact = contact;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	@Override
+	public boolean equals(Object that) {
+		if (that == null)
+			return false;
+		if (!(that instanceof AuthorTransport))
+			return false;
+		AuthorTransport thatObj = (AuthorTransport) that;
+		return Objects.equals(name, thatObj.name) && Objects.equals(contact, thatObj.contact);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, contact);
+	}
+
+}
