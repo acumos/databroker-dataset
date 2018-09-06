4b6afe01b1bbbff9d758ced8daeab384ea9d54e1
gs495a <gs495a@att.com>
2018-09-06 10:44:49 -0500

    Dataset Management
    Added CMLP dataset CRUD operation without implemenation
    Added documentation
    Added sub module for dataset-service
    Added sub module for datasource-service
    
    Issue-ID: ACUMOS-1182
    
    Change-Id: I81414d7b83fa8ac3ea9a141cd926504e39eba59e
    Signed-off-by: gs495a <gs495a@att.com>

---
+++ dataset-service/src/main/java/org/acumos/service/schema/DatasetTransformationInfo.java
@@ -0,0 +1,50 @@
+/*-
+ * ===============LICENSE_START=======================================================
+ * Acumos
+ * ===================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ===================================================================================
+ * This Acumos software file is distributed by AT&T
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============LICENSE_END=========================================================
+ */
+
+package org.acumos.service.schema;
+
+public class DatasetTransformationInfo {
+	
+	protected int columnIndex;
+	protected String type;
+	protected String expression;
+	
+	
+	public int getColumnIndex() {
+		return columnIndex;
+	}
+	public void setColumnIndex(int columnIndex) {
+		this.columnIndex = columnIndex;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getExpression() {
+		return expression;
+	}
+	public void setExpression(String expression) {
+		this.expression = expression;
+	}
+	
+	
+}
---
+++ docs/release-notes.rst
@@ -0,0 +1,29 @@
+.. ===============LICENSE_START=======================================================
+.. Acumos CC-BY-4.0
+.. ===================================================================================
+.. Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+.. ===================================================================================
+.. This Acumos documentation file is distributed by AT&T
+.. under the Creative Commons Attribution 4.0 International License (the "License");
+.. you may not use this file except in compliance with the License.
+.. You may obtain a copy of the License at
+..
+.. http://creativecommons.org/licenses/by/4.0
+..
+.. This file is distributed on an "AS IS" BASIS,
+.. WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+.. See the License for the specific language governing permissions and
+.. limitations under the License.
+.. ===============LICENSE_END=========================================================
+
+=================================
+Dataset Service Release Notes
+=================================
+
+The service is deployed as a jar file to a Maven Nexus repository. The server is deployed as a Docker image to a Docker registry.
+
+Version 1.17.3, 31 Aug 2018
+---------------------------
+
+* Added new API endpoint to get Dataset info, right now it is just prototype (ACUMOS-1182)
+
