dc7d9ae2906173a3ca67f4cf19a60c05f74307aa
gs495a <gs495a@att.com>
2018-09-06 10:44:49 -0500

    Dataset Management
    Added CMLP dataset CRUD operation without implemenation
    Added documentation
    Added sub module for dataset-service
    Added sub module for datasource-service
    
    Issue-ID: ACUMOS-1182
    
    Change-Id: I81414d7b83fa8ac3ea9a141cd926504e39eba59e
    Signed-off-by: gs495a <gs495a@att.com>

---
+++ datasource-service/src/main/java/org/acumos/service/controller/DatasourceFileController.java
@@ -0,0 +1,74 @@
+/*-
+ * ===============LICENSE_START=======================================================
+ * Acumos
+ * ===================================================================================
+ * Copyright (C) 2018 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+ * ===================================================================================
+ * This Acumos software file is distributed by AT&T and Tech Mahindra
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============LICENSE_END=========================================================
+ */
+
+package org.acumos.service.controller;
+
+import java.lang.invoke.MethodHandles;
+import java.util.List;
+
+import org.acumos.service.schema.FileDetailsInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Controller
+@RequestMapping(value = "/"+DatasourceFileController.DATASOURCES_FILES, produces = MediaType.APPLICATION_JSON_VALUE)
+public class DatasourceFileController {
+	
+	private static final String KEYTABFILE = "keytabfile";
+	private static final String CONFIGFILE = "configfile";
+	static final String DATASOURCES_FILES = "datasources/files";
+	private static final String INTERNAL_SERVER_ERROR = "Internal Server Error";
+	private static final String NOT_FOUND = "Not Found";
+	private static final String FORBIDDEN = "Forbidden";
+	private static final String UNAUTHORIZED = "Unauthorized";
+	private static final String BAD_REQUEST = "Bad Request";
+	private static final String OK = "OK";
+	private static final String RETURNS_A_JSON_STRING_THAT_NOTIFIES_STATUS_OF_UPLOADING_FILES = "Returns a JSON string that notifies status of uploading files.";
+	private static final String RESPOND_WITH_STATUS_OF_UPLOADING_KERBEROS_CONFIG_FILES = "Respond with status of uploading kerberos config files.";
+	
+	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+	
+	@RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiOperation(value = RESPOND_WITH_STATUS_OF_UPLOADING_KERBEROS_CONFIG_FILES, notes = RETURNS_A_JSON_STRING_THAT_NOTIFIES_STATUS_OF_UPLOADING_FILES, response = FileDetailsInfo.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = OK),
+			@ApiResponse(code = 400, message = BAD_REQUEST),
+			@ApiResponse(code = 401, message = UNAUTHORIZED),
+			@ApiResponse(code = 403, message = FORBIDDEN),
+			@ApiResponse(code = 404, message = NOT_FOUND),
+			@ApiResponse(code = 500, message = INTERNAL_SERVER_ERROR) })
+	public FileDetailsInfo kerberosFileupload(
+			@RequestParam(CONFIGFILE) List<RequestPart> bodyParts1,
+			@RequestParam(KEYTABFILE) List<RequestPart> bodyParts2) throws Exception {
+		
+		return new FileDetailsInfo();
+	}
+
+}
---
+++ datasource-service/src/test/java/org/acumos/test/controller/DatasourceControllerTest.java
@@ -0,0 +1,149 @@
+/*-
+ * ===============LICENSE_START=======================================================
+ * Acumos
+ * ===================================================================================
+ * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * ===================================================================================
+ * This Acumos software file is distributed by AT&T
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============LICENSE_END=========================================================
+ */
+
+package org.acumos.test.controller;
+
+import static org.mockito.Mockito.mock;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+import java.lang.invoke.MethodHandles;
+import java.util.List;
+
+import org.acumos.service.controller.DatasourceController;
+import org.acumos.service.schema.DataSourceMetadata;
+import org.acumos.service.schema.DataSourceModel;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DatasourceControllerTest {
+
+	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Mock
+	private Environment env;
+
+	@InjectMocks
+	private DatasourceController datasourceController;
+
+	@Before
+	public void createClient() throws Exception {
+		mockMvc = standaloneSetup(datasourceController).build();
+	}
+	
+	@Test
+	public void getDataSourcesList() {
+		datasourceController = mock(DatasourceController.class);
+		List<DataSourceModel> list = datasourceController.getDataSourcesList("namespace", "category", "textSearch", 0, 0);
+		logger.info("Successfully fetch: " + list.toString());
+		Assert.assertNotNull(list);
+	}
+	
+	@Test
+	public void getDataSource() {
+		datasourceController = mock(DatasourceController.class);
+		DataSourceModel list = datasourceController.getDataSource("datasourceKey");
+		logger.info("Successfully fetch: " + list.toString());
+		Assert.assertNotNull(list);
+	}
+	
+	@Test
+	public void getDataSourceContents() {
+		datasourceController = mock(DatasourceController.class);
+		String list = datasourceController.getDataSourceContents("dataSourceKey", "hdfsFilename", "proxyHost", 0, "proxyUsername", "proxyPassword");
+		logger.info("Successfully fetch: " + list);
+		Assert.assertNotNull(list);
+	}
+	
+	@Test
+	public void getMetadata() {
+		datasourceController = mock(DatasourceController.class);
+		List<DataSourceMetadata> list = datasourceController.getMetadata("datasourceKey");
+		logger.info("Successfully fetch: " + list);
+		Assert.assertNotNull(list);
+	}
+	
+	@Test
+	public void saveDataSourceDetail() throws Exception {
+		datasourceController = mock(DatasourceController.class);
+		DataSourceModel list = datasourceController.saveDataSourceDetail(null, "proxyHost", 0, "proxyUsername", "proxyPassword");
+		logger.info("Successfully fetch: " + list);
+		Assert.assertNotNull(list);
+	}
+	
+	@Test
+	public void updateDataSourceDetail(){
+		datasourceController = mock(DatasourceController.class);
+		DataSourceModel list = datasourceController.updateDataSourceDetail("dataSourcekey", null, "proxyHost", 0, "proxyUsername", "proxyPassword");
+		logger.info("Successfully fetch: " + list);
+		Assert.assertNotNull(list);
+	}
+	
+	@Test
+	public void deleteDataSourceDetail() {
+		datasourceController = mock(DatasourceController.class);
+		DataSourceModel list = datasourceController.deleteDataSourceDetail("dataSourcekey");
+		logger.info("Successfully fetch: " + list);
+		Assert.assertNotNull(list);
+	}
+	
+	@Test
+	public void validateDataSourceConnection() {
+		datasourceController = mock(DatasourceController.class);
+		DataSourceModel list = datasourceController.validateDataSourceConnection("dataSourcekey");
+		logger.info("Successfully fetch: " + list);
+		Assert.assertNotNull(list);
+	}
+	
+	@Test
+	public void getDataSourceSamples() {
+		datasourceController = mock(DatasourceController.class);
+		String list = datasourceController.getDataSourceSamples("dataSourcekey",null);
+		logger.info("Successfully fetch: " + list);
+		Assert.assertNotNull(list);
+	}
+	
+	@Test
+	public void writebackPrediction() {
+		datasourceController = mock(DatasourceController.class);
+		DataSourceModel list = datasourceController.writebackPrediction("dataSourcekey", "hdfsFilename", "includesHeader", "data", "proxyHost", 0, "proxyUsername", "proxyPassword");
+		logger.info("Successfully fetch: " + list);
+		Assert.assertNotNull(list);
+	}
+	
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
